7c68d9d5a59986cfcae5d12b81c04f286dfeb7ba
a.oslina <Anastasia.Oslina@urfu.me>
2024-12-25 14:31:38 +0500

    Add tests

---
+++ TagsCloudVisualizationTests/FileReaderFactoryTests.cs
@@ -0,0 +1,60 @@
+﻿using Autofac;
+using FluentAssertions;
+using TagsCloudVisualization;
+using TagsCloudVisualization.ConsoleCommands;
+using TagsCloudVisualization.FileReaders;
+
+namespace TagsCloudVisualizationTests;
+
+[TestFixture]
+public class FileReaderFactoryTests
+{
+    private ILifetimeScope Scope { get; set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        var options = new Options
+        {
+            InputFilePath = "/path/in/test/not/needed",
+            OutputDirectory = "/path/in/test/not/needed",
+        };
+        var container = ContainerConfig.Configure(options);
+        Scope = container.BeginLifetimeScope();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Scope.Dispose();
+    }
+
+    [TestCase("source\\input.txt", typeof(TextFileReader))]
+    [TestCase("source\\input.doc", typeof(DocFileReader))]
+    [TestCase("source\\input.docx", typeof(DocxFileReader))]
+    public void GetFileReader_ShouldReturnCorrectFileReader(string path, Type expectedType)
+    {
+        var absolutePath = Path.GetFullPath(path);
+        var fileReaderFactory = Scope.Resolve<FileReaderFactory>();
+
+        fileReaderFactory.GetFileReader(absolutePath).Should().BeOfType(expectedType);
+    }
+
+    [Test]
+    public void GetFileReader_ShouldThrowFileNotFoundException_WhenFileDoesNotExist()
+    {
+        var fileReaderFactory = Scope.Resolve<FileReaderFactory>();
+        var action = () => fileReaderFactory.GetFileReader("any/path");
+        
+        action.Should().Throw<FileNotFoundException>();
+    }
+
+    [Test]
+    public void GetFileReader_ShouldThrowNotSupportedException_WhenFileIsNotSupported()
+    {
+        var fileReaderFactory = Scope.Resolve<FileReaderFactory>();
+        var action = () => fileReaderFactory.GetFileReader("Utilities\\mystem.exe");
+        
+        action.Should().Throw<NotSupportedException>();
+    }
+}
---
+++ TagsCloudVisualizationTests/WordPreprocessorTests.cs
@@ -0,0 +1,71 @@
+﻿using Autofac;
+using FluentAssertions;
+using TagsCloudVisualization;
+using TagsCloudVisualization.ConsoleCommands;
+using TagsCloudVisualization.FileReaders;
+using TagsCloudVisualization.WordPreprocessors;
+
+namespace TagsCloudVisualizationTests;
+
+[TestFixture]
+public class WordPreprocessorTests
+{
+    private static readonly string PathToTestFile = Path.GetFullPath("source\\input.txt");
+    private ILifetimeScope Scope { get; set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        var options = new Options
+        {
+            InputFilePath = PathToTestFile,
+            OutputDirectory = "/path/in/test/not/needed",
+        };
+        var container = ContainerConfig.Configure(options);
+        Scope = container.BeginLifetimeScope();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Scope.Dispose();
+    }
+
+    [Test]
+    public void ProcessTextToWords_ShouldContainOnlyNotBoringWordsReducedToInitialFormWithLowerCase()
+    {
+        var words = ReadTestFile();
+        var result = new List<Tuple<string, int>>
+        {
+            new("кошка", 2),
+            new("собака", 3)
+        };
+
+        var wordProcessor = Scope.Resolve<IWordPreprocessor>();
+        wordProcessor.ProcessTextToWords(words).Should().BeEquivalentTo(result);
+
+    }
+
+    [Test]
+    public void ProcessTextToWords_ShouldContainCorrectCountOfWords()
+    {
+        var words = ReadTestFile();
+        
+        var wordProcessor = Scope.Resolve<IWordPreprocessor>();
+        wordProcessor.ProcessTextToWords(words).Should().HaveCount(2);
+    }
+
+    [Test]
+    public void ProcessTextToWords_ShouldBeOrderedByNumberOfWords()
+    {
+        var words = ReadTestFile();
+        
+        var wordProcessor = Scope.Resolve<IWordPreprocessor>();
+        wordProcessor.ProcessTextToWords(words).Should().BeInDescendingOrder(word => word.Item2);
+    }
+
+    private string ReadTestFile() => 
+        Scope.Resolve<FileReaderFactory>()
+            .GetFileReader(PathToTestFile)
+            .Read(PathToTestFile);
+}
